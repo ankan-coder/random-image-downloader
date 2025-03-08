ec6f3405a671d9061ee0b18dffe7aa1eedc1ee00
Ankan <76089611+ankan-coder@users.noreply.github.com>
2025-03-08 23:36:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def home():
     return jsonify({"message": "Welcome to the Image Downloader API!"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
